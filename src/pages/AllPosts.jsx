53e8a7ff6098b8fb156455d5267733fe98082f19
asyncdoggo <ayushdeshpande81@gmail.com>
2023-04-01 14:14:24 +0530

    Add authenticated get posts

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,3 +1,4 @@
+import { auth } from "../auth/firebase";
 import PostList from "../components/posts/PostList";
 import classes from "./AllPosts.module.css";
 import { useRef, useState } from "react";
@@ -7,28 +8,31 @@ const AllPostsPage = () => {
   const [isLoading, setLoading] = useState(true);
   const [loadedPosts, setLoadedPosts] = useState([]);
 
+
   const fetchData = () => {
     setLoading(true);
-    fetch(
-      "https://community-hub-4-default-rtdb.firebaseio.com/posts.json"
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        var posts = [];
-        for (const key in data) {
-          const post = {
-            id: key,
-            ...data[key],
-          };
-          posts.push(post);
-        }
-        posts = posts.reverse();
-        console.log(posts);
-        setLoadedPosts(posts);
-        setLoading(false);
-      });
+    auth.currentUser.getIdToken(/* forceRefresh */ true).then(async function (idToken) {
+
+      fetch(
+        `https://community-hub-4-default-rtdb.firebaseio.com/posts.json?auth=${idToken}`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          var posts = [];
+          for (const key in data) {
+            const post = {
+              id: key,
+              ...data[key],
+            };
+            posts.push(post);
+          }
+          posts = posts.reverse();
+          setLoadedPosts(posts);
+          setLoading(false);
+        });
+    })
   };
 
   useEffect(fetchData, []);
